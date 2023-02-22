6658300d8bf30f3442bf1bff4e64335c1b76f35c
aleks vangjelofski <aleks.vangjelofski@gmail.com>
2023-02-22 17:18:56 +0100

    Added forms and controllers

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,5 +5,5 @@ class Restaurant < ApplicationRecord
   # A restaurant must have a name, an address and a category.
   validates :name, :address, :category, presence: true
   # A restaurant’s category must belong to this fixed list: ["chinese", "italian", "japanese", "french", "belgian"].
-  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian) }
 end
